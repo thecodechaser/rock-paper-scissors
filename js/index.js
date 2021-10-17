22d04b06ecc00f469b8ed7e8f11c1900b0b4981f
Ranjeet Singh <83803414+thecodechaser@users.noreply.github.com>
2021-10-17 18:29:17 +0530

    modify javascript -add playRound() function

--- js/index.js
+++ js/index.js
@@ -18,3 +18,46 @@ let playerScore=0;
 let computerScore=0;
 
 
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection) {
+        return "It is a tie";
+    }
+
+
+    if(playerSelection == "rock") {
+
+        if(computerSelection == "scissors") {
+            playerScore++;
+            return "You Win! Rock beats Scissors";
+        } else if(computerSelection == "paper") {
+            computerScore++;
+            return "You Lose! Paper beats Rock";
+        }
+    }
+
+    if(playerSelection == "paper") {
+
+        if(computerSelection == "rock") {
+            playerScore++;
+            return "You Win! Paper beats Rock";
+        } else if(computerSelection == "scissors") {
+            computerScore++;
+            return "You Lose! Scissors beats Paper";
+        }
+    }
+
+    if(playerSelection == "scissors") {
+
+        if(computerSelection == "paper") {
+            playerScore++;
+            return "You Win! Scissors beats Paper";
+        } else if(computerSelection == "rock") {
+            computerScore++;
+            return "You Lose! Rock beats Scissors";
+        }
+    } else {
+        return "Wrong input! please choose among Rock, Paper, Scissors"
+    }
+}
+
+
